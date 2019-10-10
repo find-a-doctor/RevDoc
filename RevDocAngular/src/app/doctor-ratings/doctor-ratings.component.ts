dbf62af29019ef7e1991b0669b1540f0fca1266c
Aaron <gauntta@gmail.com>
2019-10-10 16:44:45 -0500

    adding base angular project

---
+++ RevDocAngular/src/app/doctor-ratings/doctor-ratings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-ratings',
+  templateUrl: './doctor-ratings.component.html',
+  styleUrls: ['./doctor-ratings.component.css']
+})
+export class DoctorRatingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
