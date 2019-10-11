785d6a80daebaafe517b09355b82bb13bfc34425
Jessica <jessica.archerd@gmail.com>
2019-10-11 16:26:15 -0500

    added DoctorController to populate doctor profile page, wrote DoctorDAO, DoctorInfoService, and DoctorInfoServiceImpl to access database and populate components

--- Project3/revdoc_schedule/src/main/java/com/revdoc/dao/DoctorDAO.java
+++ Project3/revdoc_schedule/src/main/java/com/revdoc/dao/DoctorDAO.java
@@ -1,6 +1,7 @@
 package com.revdoc.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revdoc.model.Doctor;
@@ -8,4 +9,9 @@ import com.revdoc.model.Doctor;
 @Repository
 public interface DoctorDAO  extends JpaRepository<Doctor, Long>{
 
+	@Query("select d from Doctor d where d.npi = :npi")
+	Doctor findByNpi(long npi);
+
+
+	
 }
