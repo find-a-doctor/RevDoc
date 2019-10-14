5946b1bee5af8fea8e9410087d08d00053d0b6c6
Jessica <jessica.archerd@gmail.com>
2019-10-14 14:30:39 -0500

    added DAO

--- RevDocAngular/src/app/app-routing.module.ts
+++ RevDocAngular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ex-dr-page', component: DoctorProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RevDocAngular/src/app/app.module.ts
+++ RevDocAngular/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { FollowDoctorComponent } from './follow-doctor/follow-doctor.component';
 import { DoctorRatingsComponent } from './doctor-ratings/doctor-ratings.component';
 import { DoctorFeedbackComponent } from './doctor-feedback/doctor-feedback.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
