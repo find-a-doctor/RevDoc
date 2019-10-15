74a0b80acc4280dac7b7604eb79888d0360d97c1
Aaron <gauntta@gmail.com>
2019-10-15 11:56:15 -0500

    working on displaying doctor appointments

--- RevDocAngular/src/app/app-routing.module.ts
+++ RevDocAngular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchDoctorComponent } from './search-doctor/search-doctor.component';
+import { ViewApptComponent } from './view-appt/view-appt.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'searchDoctor',  pathMatch: 'full' },
+  { path: 'searchDoctor', component: SearchDoctorComponent},
+  { path: 'viewSchedule', component: ViewApptComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RevDocAngular/src/app/view-appt.service.ts
+++ RevDocAngular/src/app/view-appt.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewApptService {
+  public grabUserUrl: string;
+  constructor(private http: HttpClient) {
+    this.grabUserUrl = "http://localhost:9000/grabUser/";
+   }
 
-  constructor() { }
+   public grabUser(search: string) {
+    console.log("Grab User: " + this.grabUserUrl + search);
+    //  return this.http.get<Doctor[]>(this.searchDoctorUrl+search);
+    return this.http.get<Object[]>(this.grabUserUrl + search);
+  }
 }
