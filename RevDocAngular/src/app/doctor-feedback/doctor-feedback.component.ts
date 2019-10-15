3e69bfd1bbad9b272dbbd5af70e4078e36aa0019
Ethan <etbaier9@gmail.com>
2019-10-15 17:13:43 -0500

    changed radio buttons to reflect star images. onhover added to show orange star, but layout still needs work.

--- RevDocAngular/src/app/doctor-feedback/doctor-feedback.component.ts
+++ RevDocAngular/src/app/doctor-feedback/doctor-feedback.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Feedback } from '../revdoc-classes/feedback'; 
+import { Component, OnInit } from 'node_modules/@angular/core';
+import { Feedback } from '../revdoc-classes/feedback';
+import { ActivatedRoute, Router } from 'node_modules/@angular/router';
+
 
 
 @Component({
@@ -9,16 +11,22 @@ import {Feedback } from '../revdoc-classes/feedback';
 })
 export class DoctorFeedbackComponent implements OnInit {
 
- 
-  constructor(feeback :Feedback) { }
+  feedback: Feedback;
+  // constructor(feeback: Feedback) { }
 
   ngOnInit() {
   }
-  
-  
 
-  rateDoctor(BM : number, WT : number, OV : number, Comment : string){
-    console.log("Bedside = "+BM+" WT = "+WT+" Overall = "+OV+" Comment = "+Comment);
+
+  //Collects ratings and comment from user to create a feedback object then sends an alert to user that the feedback has been recieved.
+  rateDoctor(BM: number, WT: number, OV: number, Comment: string) {
+
+    console.log("Bedside = " + BM + " WT = " + WT + " Overall = " + OV + " Comment = " + Comment);
+    this.feedback.bedsideMannerRating = BM;
+    this.feedback.waitTimeRating = WT;
+    this.feedback.overallRating = OV;
+    this.feedback.comments = Comment;
+    console.log("Feedback Object: "+this.feedback);
     alert("Thank you for your feedback!")
   }
 
