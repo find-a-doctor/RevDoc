dea1a8d5a014b2f974e8d65744ef5550c84fab9e
Courtney <CourtneyStarling22@gmail.com>
2019-10-16 09:32:59 -0500

    not yet working

--- RevDocAngular/src/app/doctor-info.service.ts
+++ RevDocAngular/src/app/doctor-info.service.ts
@@ -3,31 +3,37 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Doctor } from './revdoc-classes/doctor';
 import { Router } from '@angular/router';
-
+import { Followers } from './revdoc-classes/followers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorInfoService {
 
-  baseUrl:string;
-  doctorUrl:string;
-  getAllUrl:string;
+  baseUrl: string;
+  doctorUrl: string;
+  getAllUrl: string;
+  followingUrl: string;
 
-  constructor(private http:HttpClient, private router:Router) {
-    this.baseUrl="http://localhost:1000/";
-    this.doctorUrl=this.baseUrl+"doctor/";
-    this.getAllUrl=this.baseUrl+"doctors";
+  constructor(private http: HttpClient, private router: Router) {
+    this.baseUrl = "http://localhost:1000/";
+    this.doctorUrl = this.baseUrl + "doctor/";
+    this.getAllUrl = this.baseUrl + "doctors";
+    this.followingUrl = this.baseUrl +"following/"
 
 
    }
 
-   public getDoctor(npi:number):Observable<Doctor>{
-     return this.http.get<Doctor>(this.doctorUrl+npi);
-   }
+  public getDoctor(npi: number): Observable<Doctor> {
+    return this.http.get<Doctor>(this.doctorUrl + npi);
+  }
 
-   public getAllDoctors():Observable<Doctor[]>{
-     return this.http.get<Doctor[]>(this.getAllUrl);
-   }
+  public getAllDoctors(): Observable<Doctor[]> {
+    return this.http.get<Doctor[]>(this.getAllUrl);
+  }
+
+  public isFollowing(npi: number, revassociate: string): Observable<boolean> {
+    return this.http.get<boolean>(this.followingUrl+npi+"/"+revassociate);
+  }
 
 }
--- RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
+++ RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DoctorInfoService } from '../doctor-info.service';
+import { Followers } from '../revdoc-classes/followers';
 
 @Component({
   selector: 'app-follow-doctor',
   templateUrl: './follow-doctor.component.html',
   styleUrls: ['./follow-doctor.component.css']
 })
-export class FollowDoctorComponent implements OnInit {
+export class FollowDoctorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  followers: Followers;
+
+  npi:number;
+  revassociate:string;
+  followdate:number;
+
+ 
+  isFollowing: boolean;
+
+  following;
+  
+  constructor(private doctorInfoService: DoctorInfoService) { }
 
   ngOnInit() {
+
+    //check if revassoc is following this doc
+    this.doctorInfoService.isFollowing(this.npi, this.revassociate).subscribe(data => {
+      this.isFollowing = data;
+      console.log("following data: "+this.isFollowing);
+    }
+    
+  toggleFollow() {
+    const npi = this.npi
+    const revassociateemail = this.revassociateemail
+
+    if (this.isFollowing) this.doctorInfoService.unfollow(revassociateemail, npi)
+    else this.doctorInfoService.follow(revassociateemail, npi)
   }
 
+  ngOnDestroy() {
+    this.following.unsubscribe();
+  }
 }
