325963dfe3b07bc25e6c1ad8f162991484651895
Aaron <gauntta@gmail.com>
2019-10-16 10:39:29 -0500

    normalizing Angular Http calls

--- RevDocAngular/src/app/view-appt.service.ts
+++ RevDocAngular/src/app/view-appt.service.ts
@@ -7,18 +7,28 @@ import { HttpClient } from '@angular/common/http';
 
 export class ViewApptService {
 
-  public grabUserUrl: string;
-  public grabUserApptsUrl: string;
-  public grabDoctorApptsUrl: string;
+  public allApptsUrl: string;
+  public userApptsUrl: string;
+  public doctorApptsUrl: string;
 
   constructor(private http: HttpClient) {
-    this.grabUserUrl = "http://localhost:9000/grabUser/";
-    this.grabUserApptsUrl = "http://localhost:9000/appointments";
-    this.grabDoctorApptsUrl = "http://localhost:9000/doctorAppointments";
+    this.allApptsUrl = "http://localhost:9000/appointments/";
+    this.userApptsUrl = "http://localhost:9000/appointments";
+    this.doctorApptsUrl = "http://localhost:9000/doctorAppointments";
    }
 
-   public grabUser(email: string) {
-    console.log("Grab User: " + this.grabUserUrl + email);
-    return this.http.get<Object[]>(this.grabUserUrl + email);
+   public allAppts() {
+    console.log("Grabbing all Appts: " + this.allApptsUrl);
+    return this.http.get<Object[]>(this.allApptsUrl);
+  }
+
+  public userAppts(email: string) {
+    console.log("Grabbing all Appts: " + this.userApptsUrl + email);
+    return this.http.get<Object[]>(this.userApptsUrl + email);
+  }
+
+  public doctorAppts(email: string) {
+    console.log("Grabbing all Appts: " + this.doctorApptsUrl + email);
+    return this.http.get<Object[]>(this.doctorApptsUrl + email);
   }
 }
