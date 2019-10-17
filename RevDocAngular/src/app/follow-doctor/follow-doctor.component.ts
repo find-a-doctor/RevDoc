ee0445b522d5390a65cd44fafd12b624db211cd4
Jessica <jessica.archerd@gmail.com>
2019-10-17 09:35:42 -0500

    integrated follow component to doctor profile, moved follow and unfollow methods out of ngOnInit()

--- RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
+++ RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DoctorInfoService } from '../doctor-info.service';
+import { Followers } from '../revdoc-classes/followers';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-follow-doctor',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowDoctorComponent implements OnInit {
 
-  constructor() { }
+  followers: Followers;
+
+  npi: number;
+  revassociate: string;
+  followdate: number;
+  followerId: number;
+
+  isFollowing: boolean;
+
+  // followersObject: Followers;
+
+  // getAllFollowers: Followers;
+
+
+  constructor(private doctorInfoService: DoctorInfoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    //check if revassoc is following this doc
+    this.doctorInfoService.isFollowing(this.npi, this.revassociate).subscribe(data => {
+      this.isFollowing = data;
+      console.log("is following data boolean: " + this.isFollowing);
+    });
+  }
+
+  public followDoctor() {
+    this.doctorInfoService.followDoctor(this.followers).subscribe(data => {
+      this.followers = data;
+      console.log("follow doctor data: " + this.followDoctor);
+    });
+  }
+
+  public unfollowDoctor() {
+    this.doctorInfoService.unfollowDoctor(this.followerId);
   }
 
 }
