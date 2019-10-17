db6b65692ea190de394592cbf58b14cb67879af0
Jessica <jessica.archerd@gmail.com>
2019-10-17 10:10:11 -0500

    successfully checks if the user is following the doctor

--- RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
+++ RevDocAngular/src/app/follow-doctor/follow-doctor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DoctorInfoService } from '../doctor-info.service';
 import { Followers } from '../revdoc-classes/followers';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RevAssociate } from '../revdoc-classes/rev-associate';
 
 @Component({
   selector: 'app-follow-doctor',
@@ -13,25 +14,39 @@ export class FollowDoctorComponent implements OnInit {
   followers: Followers;
 
   npi: number;
-  revassociate: string;
+  // revassociate: string;
+  revassociate:RevAssociate;
   followdate: number;
   followerId: number;
 
   isFollowing: boolean;
 
-  // followersObject: Followers;
 
-  // getAllFollowers: Followers;
+  constructor(private doctorInfoService: DoctorInfoService, private route: ActivatedRoute, private router: Router) {
 
 
-  constructor(private doctorInfoService: DoctorInfoService, private route: ActivatedRoute, private router: Router) { }
+  }
 
   ngOnInit() {
+    //dummy user, hardcoded for pre-integrations
+    this.revassociate = new RevAssociate();
+    this.revassociate.revAssociateEmail="sampleuser@rev.com";
+    this.revassociate.revAssociateName="Mr Duck";
+
+    //doctor found from url
+    // console.log(this.route.url);
+    this.route.url.subscribe(data=>{
+      // console.log(data);
+      this.npi=Number(data[1]);
+    })
+    // console.log("npi: "+this.npi)
+
     //check if revassoc is following this doc
-    this.doctorInfoService.isFollowing(this.npi, this.revassociate).subscribe(data => {
-      this.isFollowing = data;
-      console.log("is following data boolean: " + this.isFollowing);
+    this.doctorInfoService.isFollowing(this.npi, this.revassociate.revAssociateEmail).subscribe(data => {
+      console.log("isfollowing?"+data);
+      this.isFollowing = Boolean(data);
     });
+    console.log("is following data boolean: " + this.isFollowing);
   }
 
   public followDoctor() {
