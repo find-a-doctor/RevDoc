92f043f8e87db48d33050877eea52d23f3fa47c2
Jessica <jessica.archerd@gmail.com>
2019-10-23 18:55:53 -0500

    working

--- RevDocAngular/src/app/doctor-ratings/doctor-ratings.component.ts
+++ RevDocAngular/src/app/doctor-ratings/doctor-ratings.component.ts
@@ -25,40 +25,38 @@ export class DoctorRatingsComponent implements OnInit {
     this.doctor = new Doctor();
     this.route.url.subscribe(data => {
       this.doctor.npi = Number(data[1].path);
-    });
 
+      this.doctorInfoService.getDoctor(this.doctor.npi).subscribe(data => {
+        // console.log("getting...\n" + data);
+        this.doctor = data;
 
-    this.doctorInfoService.getDoctor(this.doctor.npi).subscribe(data => {
-      // console.log("getting...\n" + data);
-      this.doctor = data;
-    }, error => console.log("error:\n" + error));
+        this.doctorInfoService.getAllRatings(this.doctor.npi).subscribe(data => {
+          var ov: number = 5;
+          var bm: number = 5;
+          var wt: number = 5;
+          var count: number = 0;
+          this.allFeedback = data;
+          this.allFeedback.forEach(function (fb: Feedback) {
+            // ov += fb.overallRating;
+            bm += fb.bedsideMannerRating;
+            wt += fb.waitTimeRating;
+            count += 1;
+          });
+    
+          // this.overall = (ov / count);
+          if (count == 0) {
+            console.log("no reviews available")
+            count = 1;
+          }
+    
+          this.bedsideManner = (bm / count);
+          this.waitTime = (wt / count);
+          this.overall = (ov / count);
+        });
 
-    this.doctorInfoService.getAllRatings(this.doctor.npi).subscribe(data => {
-      var ov: number = 5;
-      var bm: number = 5;
-      var wt: number = 5;
-      var count: number = 0;
-      this.allFeedback = data;
-      this.allFeedback.forEach(function (fb: Feedback) {
-        // ov += fb.overallRating;
-        bm += fb.bedsideMannerRating;
-        wt += fb.waitTimeRating;
-        count += 1;
-      });
-
-      // this.overall = (ov / count);
-      if (count == 0) {
-        console.log("no reviews available")
-        count = 1;
-      }
-
-      this.bedsideManner = (bm / count);
-      this.waitTime = (wt / count);
-      this.overall = (ov / count);
-
-
-      console.log(this.overall);
+      }, error => console.log("error:\n" + error)); 
     });
 
   }
+
 }
