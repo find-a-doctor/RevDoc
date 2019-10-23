ee9119816dc93f368d123d388c0a35c4d8640766
Aaron <gauntta@gmail.com>
2019-10-22 20:05:33 -0500

    adding lost changes to login-page

--- RevDocAngular/src/app/session.service.ts
+++ RevDocAngular/src/app/session.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RevAssociate } from './revdoc-classes/rev-associate';
+import { Doctor } from './revdoc-classes/doctor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+  public associateSessionUrl: string;
+  public doctorSessionUrl: string;
+  public getAssociateSessionUrl: string;
+  public getDoctorSessionUrl: string;
+  public destroySessionUrl: string
+  public associate: RevAssociate;
+  public doctor: Doctor;
+
+  constructor(private http: HttpClient) {
+    this.associateSessionUrl = "http://localhost:9000/associateSession";
+    this.doctorSessionUrl= "http://localhost:9000/doctorSession";
+    this.getAssociateSessionUrl = "http://localhost:9000/getAssociateSession"
+    this.getDoctorSessionUrl = "http://localhost:9000/getDocSession"
+    this.destroySessionUrl = "http://localhost:9000/destroy"
+   }
+
+   public initAssociateSession(associate: RevAssociate){
+     return this.http.post<RevAssociate>(this.associateSessionUrl, associate)
+   }
+
+   public initDoctorSession(doctor: Doctor) {
+     return this.http.post<Doctor>(this.doctorSessionUrl, doctor)
+   }
+
+   public getAssociateSession(){
+     return this.http.get<RevAssociate>(this.getAssociateSessionUrl)
+   }
+
+   public getDoctorSession(){
+    return this.http.get<Doctor>(this.getDoctorSessionUrl)
+  }
 }
